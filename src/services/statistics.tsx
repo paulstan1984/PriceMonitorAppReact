2bbc241977a26df3dd908b87765aedc033b108dd
Paul Stan <Paul Stan>
2022-08-19 16:19:31 +0300

    Update statistics.tsx

---
+++ src/services/statistics.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { AppConfigs } from "../config";
+
+class StatisticsService {
+
+    private static headers = {
+        'Content-Type': 'application/json',
+        'Authorization': ''
+    }
+
+    public static GetMonthlyStats() {
+        
+    }
+
+    public static GetDailyStats() {
+        
+    }
+
+    public static GetDailyDetails(day: Date) {
+        
+    }
+}
+
+export default StatisticsService;
