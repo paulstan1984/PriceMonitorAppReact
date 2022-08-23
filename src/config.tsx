ca19443d8f212d1cfea6c0afc0c6968d863cb02b
Paul Stan <Paul Stan>
2022-08-23 20:31:15 +0300

    Update package-lock.json, price-monitor.svg, and 3 more files...

--- src/config.tsx
+++ src/config.tsx
@@ -4,7 +4,8 @@ export const AppConfigs = {
     AuthToken: 'ceadd38b63f32c6cbbbe7795e5498ce3',
     PriceMonitorToken: 'AuthToken',
 
-    ApiURL: 'http://localhost/price-monitor/api/public/',
+//    ApiURL: 'http://localhost/price-monitor/api/public/',
+    ApiURL: 'https://price.priscilla-consulting.ro/public/',
 
     DashboardRoute: 'dashboard',
     LoginRoute: 'login',
