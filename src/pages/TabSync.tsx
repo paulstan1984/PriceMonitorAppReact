1b47270ae64a9b8f522d3f3dbf110cc145c4b3cb
Paul Stan <Paul Stan>
2022-09-30 10:26:57 +0300

    Update TabSync.tsx and helpers.tsx

--- src/pages/TabSync.tsx
+++ src/pages/TabSync.tsx
@@ -1,16 +1,37 @@
-import { IonButton, IonContent, IonHeader, IonIcon, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonInput, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import FileSaver from 'file-saver';
 import { cloudDownloadOutline, folderOpenOutline } from 'ionicons/icons';
+import { appDatabase } from '../services/database';
+import { Helpers } from '../services/helpers';
 
 const TabSync: React.FC = () => {
 
-  async function ExportData() {
-    var file = new File(["Hello, world!\nline 2"], "hello world.txt", {type: "text/plain;charset=utf-8"});
+  async function ExportData(entity: string) {
+
+    var entities: any[] = [];
+    
+    switch(entity){
+      case 'products':
+        entities = await appDatabase.products.toArray();
+        break;
+      
+      case 'prices':
+        entities = await appDatabase.prices.toArray();
+        break;
+    }
+   
+    var csvContent = Helpers.ConvertToCSV(entities);
+
+    var file = new File([csvContent], entity + ".csv", { type: "text/plain;charset=utf-8" });
     FileSaver.saveAs(file);
   }
 
   async function ImportData() {
-    
+
+  }
+
+  function onFileChange(e: any){
+    console.log(e);
   }
 
   return (
@@ -24,9 +45,12 @@ const TabSync: React.FC = () => {
 
         <div className="modal-container">
 
-          <IonButton size="large" color="primary" onClick={() => ExportData()}><IonIcon icon={cloudDownloadOutline} /><IonLabel className="button-label"> Export as csv</IonLabel></IonButton>
+          <IonButton size="large" color="primary" onClick={() => ExportData('products')}><IonIcon icon={cloudDownloadOutline} /><IonLabel className="button-label"> Export producs as csv</IonLabel></IonButton>
+          <IonButton size="large" color="primary" onClick={() => ExportData('prices')}><IonIcon icon={cloudDownloadOutline} /><IonLabel className="button-label"> Export prices as csv</IonLabel></IonButton>
+
           <IonButton size="large" color="primary" onClick={() => ImportData()}><IonIcon icon={folderOpenOutline} /><IonLabel className="button-label"> Import frum csv</IonLabel></IonButton>
 
+          <IonInput onChange={(ev) =>onFileChange(ev)}/>
         </div>
 
 
---
+++ src/services/helpers.tsx
@@ -0,0 +1,27 @@
+
+export class Helpers {
+
+    public static ConvertToCSV(arr: any[]) {
+        const array = [Object.keys(arr[0])].concat(arr)
+
+        return array.map(it => {
+            return Object.values(it).toString()
+        }).join('\n')
+    }
+
+    public static LoadFromCsv(csvString: string): any {
+        var arr = csvString.split('\n');
+        var jsonObj = [];
+        var headers = arr[0].split(',');
+        for (var i = 1; i < arr.length; i++) {
+            var data = arr[i].split(',');
+            var obj: any = {};
+            for (var j = 0; j < data.length; j++) {
+                obj[headers[j].trim()] = data[j].trim();
+            }
+            jsonObj.push(obj);
+        }
+
+        return jsonObj;
+    }
+}
